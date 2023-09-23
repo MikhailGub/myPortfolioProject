5fe523688ef531175930ffd17a22e4593c919268
MikhailGub <mgubankov@yandex.ru>
2023-09-23 16:35:57 +0500

    added second section 'about'

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,27 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close')
+    hideElem = document.querySelector('.menu__overlay')
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active')
 });
 
 closeElem.addEventListener('click', () => {
-    menu.classList.remove('active')
+    hideContent();
 });
+
+hideElem.addEventListener('click', () => {
+    hideContent();
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+      hideContent();
+    }
+  });
+
+function hideContent () {
+    menu.classList.remove('active')
+}
+  
